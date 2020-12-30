d26faf1378b68b0968df230749bc6c8282b39ca7
honge7694 <76715487+honge7694@users.noreply.github.com>
2020-12-30 23:47:09 +0900

    2020-12-30 23:47

--- Java2012/Lesson002/src/com/swcodingschool/oop/MyDate5.java
+++ Java2012/Lesson002/src/com/swcodingschool/oop/MyDate5.java
@@ -4,9 +4,14 @@ public class MyDate5 {
 	private int day;
 	private	int month;
 	private int year;
+	private boolean isValid = true;
 	
 	MyDate5(int day, int month, int year){
+		setDay(day);
+		setMonth(month);
+		setYear(year);
 		
+		System.out.println(day + " " + month + " " + year + " " + isValid);
 	}
 	
 	public int getDay() {
@@ -14,7 +19,34 @@ public class MyDate5 {
 	}
 	
 	public void setDay(int day) {
-		this.day = day;
+		switch(month) {
+		case 1 : case 3 : case 5 : case 7 : case 8 : case 10 : case 12 :
+			if(day <= 31 && day > 0) {
+				this.day = day;
+			}else{
+				isValid = false;
+			}
+			break;
+			
+		case 2 :
+			if(day <= 28 && day > 0) {
+				this.day = day;
+			}else{
+				isValid = false;
+			}
+			break;
+			
+		case 4 : case 6 : case 9 : case 11 :
+			if(day <= 30 && day > 0) {
+				this.day = day;
+			}else {
+				isValid = false;
+			}
+			break;
+//			default를 주면 옳은 값도 false가 나옴.
+//		default :
+//			isValid = false; 
+		}
 	}
 	
 	public int getMonth() {
@@ -22,7 +54,11 @@ public class MyDate5 {
 	}
 	
 	public void setMonth(int month) {
-		this.month = month;
+		if(month > 0 && month < 13) {
+			this.month = month;
+		}else {
+			isValid = false;
+		}
 	}
 
 	public int getYear() {
@@ -33,7 +69,13 @@ public class MyDate5 {
 		this.year = year;
 	}
 	
-	
+	public String isValid() {
+		if(isValid) {
+			return "유요한 날짜입니다.";
+		}else {
+			return "유효하지 않은 날짜입니다.";
+		}
+	}
 	
 	
 }
--- Java2012/Lesson002/src/com/swcodingschool/oop/MyDateTest5.java
+++ Java2012/Lesson002/src/com/swcodingschool/oop/MyDateTest5.java
@@ -1,5 +1,10 @@
 package com.swcodingschool.oop;
 
 public class MyDateTest5 {
-
+	public static void main(String[] args) {
+		int a = 0;
+		MyDate5 date = new MyDate5(1, 12, 2020);
+		
+		System.out.println(date.isValid());
+	}
 }
