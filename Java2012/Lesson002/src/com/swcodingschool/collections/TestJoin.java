7b5a09b07303bb79451a6759c3aa537a25e4f14c
honge7694 <76715487+honge7694@users.noreply.github.com>
2021-01-07 17:49:20 +0900

    2021-01-07 17:49

---
+++ Java2012/Lesson002/src/com/swcodingschool/collections/TestJoin.java
@@ -0,0 +1,23 @@
+package com.swcodingschool.collections;
+
+public class TestJoin {
+	private String JoinID;
+	private String JoinName;
+	
+	TestJoin(){}
+	
+	TestJoin(String JoinID, String JoinName){
+		this.JoinID = JoinID;
+		this.JoinName = JoinName;
+	}
+
+	
+
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return JoinName + "님의 아이디는"
+	}
+	
+	
+}
