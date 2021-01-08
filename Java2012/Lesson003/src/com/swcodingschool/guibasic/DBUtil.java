4c6b373682c59e2ce15cc632d3ffbc2efbd5d0c0
honge7694 <76715487+honge7694@users.noreply.github.com>
2021-01-08 23:12:54 +0900

    2021-01-08 23:12

---
+++ Java2012/Lesson003/src/com/swcodingschool/guibasic/DBUtil.java
@@ -0,0 +1,47 @@
+package com.swcodingschool.guibasic;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/*
+ * 데이터베이스 연결과 관련한 메서드를 정의하는 클래스
+ */
+public class DBUtil {
+	static final String JDBCDRV = "com.mysql.cj.jdbc.Driver"; // 정해져있는 것.
+	static final String URL = "jdbc:mysql//localhost:3306/javadb";
+	static final String DBUSER = "java";
+	static final String DBPASSWD = "1234";
+	
+	static Connection dbconn = null;
+	
+	public static void DBConnect() {
+		try {
+			// step1. 드라이버 로드
+			Class.forName(JDBCDRV);
+			System.out.println("[MyMSG]JDBC 드라이버를 정상적으로 로드하였습니다.");
+			
+			// step2. 데이터베이스 연결
+			dbconn = DriverManager.getConnection(URL, DBUSER, DBPASSWD);
+			
+		} catch (ClassNotFoundException e) {
+			System.out.println("[MyMSG]JDBC 드라이버를 정상적으로 로드하지 못하였습였습니다.");
+			e.printStackTrace();
+			
+		} catch (SQLException e) {
+			System.out.println("[MyMSG]데이터베이스 연결에 실패하였습니다.");
+			e.printStackTrace();
+		}
+	} // end of DBConnect()
+	
+	public static void DBClose() {
+		try {
+			dbconn.close();
+			System.out.println("[MyMSG]데이터베이스 연결을 종료하였습니다.");
+		}catch(SQLException e){
+			System.out.println("[MyMSG]데이터베이스 연결을 종료하지 못하였습니다.");
+			e.printStackTrace();
+		}
+	} // end of DBClose()
+	
+} // end of class
