547a471c7240ce2d3d25122e036e8900c76529b2
honge7694 <76715487+honge7694@users.noreply.github.com>
2021-01-11 17:29:59 +0900

    2021-01-11 17:29

---
+++ Java2012/Lesson003/src/com/swcodingschool/guibasic/TestTreeSet431.java
@@ -0,0 +1,41 @@
+package com.swcodingschool.guibasic;
+
+
+import java.util.Iterator;
+import java.util.TreeSet;
+
+public class TestTreeSet431 {
+	private TreeSet<TestMember> tree;
+	
+	TestTreeSet431(){
+		tree = new TreeSet<TestMember>();
+	}
+	
+	public void addTestMember(TestMember member) {
+		tree.add(member);
+	}
+	
+	public boolean removeTestMember(int memberId) {
+		Iterator<TestMember> ir = tree.iterator();
+		
+		while(ir.hasNext()) {
+			TestMember member = ir.next();
+			
+			int tempId = member.getMemberId();
+			
+			if(tempId == memberId) {
+				tree.remove(member);
+				return true;
+			}
+		}
+		System.out.println(memberId + "가 존재하지 않습니다.");
+		return false;
+	}
+	
+	public void showAllMember() {
+		for(TestMember member : tree) {
+			System.out.println(member);
+		}
+		System.out.println("");
+	}
+}
