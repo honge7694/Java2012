9bbfe1c94f23336ae4451360e643ddf8a21577d2
honge7694 <76715487+honge7694@users.noreply.github.com>
2021-01-26 11:43:39 +0900

    2021-01-26 11:43

---
+++ Java2012/Lesson202/src/test/java/com/example/springboot/Lesson202/Lesson202ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboot.Lesson202;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lesson202ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
