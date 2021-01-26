64d28caa5faf9e92601f64fd5fd37c9e3d4ffb2a
honge7694 <76715487+honge7694@users.noreply.github.com>
2021-01-26 16:23:28 +0900

    2021-01-26 16:23

---
+++ Java2012/Lesson203/src/main/java/com/honge7694/Lesson203/LearnSpringSecureTestApplication.java
@@ -0,0 +1,13 @@
+package com.honge7694.Lesson203;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnSpringSecureTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnSpringSecureTestApplication.class, args);
+	}
+
+}
---
+++ Java2012/Lesson203/src/test/java/com/honge7694/Lesson203/LearnSpringSecureTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.honge7694.Lesson203;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnSpringSecureTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
