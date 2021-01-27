560d576a9659c1531c503dda07f5c09f72113d33
honge7694 <76715487+honge7694@users.noreply.github.com>
2021-01-27 16:53:17 +0900

    2021-01-27 16:53

---
+++ Java2012/Lesson240/src/test/java/com/swcodingschool/Lesson240/AccessMysqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swcodingschool.Lesson240;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccessMysqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
