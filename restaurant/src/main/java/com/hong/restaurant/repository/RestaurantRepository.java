fe6da0ec32b018c4165e51cb4ed9266588e896ed
honge7694 <76715487+honge7694@users.noreply.github.com>
2021-02-16 09:04:42 +0900

    2021-02-16 09:04

--- restaurant/src/main/java/com/hong/restaurant/repository/RestaurantRepository.java
+++ restaurant/src/main/java/com/hong/restaurant/repository/RestaurantRepository.java
@@ -2,6 +2,7 @@ package com.hong.restaurant.repository;
 
 import com.hong.restaurant.entity.Restaurant;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 
-public interface RestaurantRepository extends JpaRepository<Restaurant, Long> {
+public interface RestaurantRepository extends JpaRepository<Restaurant, Long>, QuerydslPredicateExecutor<Restaurant> {
 }
--- restaurant/src/main/java/com/hong/restaurant/service/RestaurantService.java
+++ restaurant/src/main/java/com/hong/restaurant/service/RestaurantService.java
@@ -1,7 +1,22 @@
 package com.hong.restaurant.service;
 
 import com.hong.restaurant.dto.RestaurantDTO;
+import com.hong.restaurant.entity.Restaurant;
 
 public interface RestaurantService {
-    Long register(RestaurantDTO dto);
+    Long register1(RestaurantDTO dto);
+
+    // service에서 repository로 갈 때 dto -> entity로 변환해야한다.
+    default Restaurant dtoToEntity(RestaurantDTO dto){
+        Restaurant entity = Restaurant.builder()
+                .pno(dto.getPno())
+                .pname(dto.getPname())
+                .pcost(dto.getPcost())
+                .fileName(dto.getFileName())
+                .oriFileName(dto.getOriFileName())
+                .filePath(dto.getFilePath())
+                .build();
+
+        return entity;
+    }
 }
